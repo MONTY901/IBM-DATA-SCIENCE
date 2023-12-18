16bea79a47c176dda443187ea1fdc0c522670410
MONTY901 <76875399+MONTY901@users.noreply.github.com>
2023-12-18 18:28:08 +0530

    Add files via upload

---
+++ DataScienceEcosystem1.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystems
+
+# in this notebook, DataScience Tools and Ecosystems are summerized
+
+# some of the popular Language DataScientists are use are:
+# 1. python
+# 2. R
+# 3. SQL
+# 4. c++
+# 5. Java etc ....;
+
+# some of the commonly used liabraries used by Datascientits include:
+# 1.  Tensorflow
+# 2.  Numpy
+# 3.  Scipy
+# 4.  Pandas
+# 5.  Matplotlib
+# 6.  Keras
+# 7.  Scikit Learn ....;
+
+# |DataScience Tools|
+# |---|
+# |SAS|
+# |Apache Hadoop|
+# |Tensorflow|
+
+# ### Below are few example of evaluating arithmatic operations in python
+
+# In[2]:
+
+
+#this is a common example to multiply then add numbers
+(3*5)+4
+
+
+# In[3]:
+
+
+#this will convert 200 minutes to hours by dividing by 60
+200/60
+
+
+# objectives
+# 1. List popular Languages for datascience
+# 2. List popular Liabraries for Datascience
+# 3. List of poluar Datascience tools
+# 4. How to provide comments in the cell
+
+# Author
+
+# Soumya Ranjan Rath
+
+# In[ ]:
+
+
+
+
